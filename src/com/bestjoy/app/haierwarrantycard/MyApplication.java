ffb2390a053813214ef51e44d4ff5a3f9adc1bb9
cncom <yeluosuifeng2005@gmail.com>
2014-04-29 22:38:00 +0800

    增加保修卡KY字段

--- src/com/bestjoy/app/haierwarrantycard/MyApplication.java
+++ src/com/bestjoy/app/haierwarrantycard/MyApplication.java
@@ -178,7 +178,7 @@ public class MyApplication extends Application{
     public SecurityKeyValuesObject getSecurityKeyValuesObject() {
     	if (mSecurityKeyValuesObject == null) {
     		//Here, we need to notice.
-    		new Exception("getSecurityKeyValuesObject() return null").printStackTrace();
+    		new Exception("warnning getSecurityKeyValuesObject() return null").printStackTrace();
     	}
     	return mSecurityKeyValuesObject;
     }
