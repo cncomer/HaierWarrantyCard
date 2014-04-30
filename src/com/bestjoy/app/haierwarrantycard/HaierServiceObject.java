3a60318434c81d9d9ba75b5eda5a282fa90f0b8a
chenkai <chenkai@huaqin.com>
2014-04-30 20:34:00 +0800

    支持显示产品预览图

--- src/com/bestjoy/app/haierwarrantycard/HaierServiceObject.java
+++ src/com/bestjoy/app/haierwarrantycard/HaierServiceObject.java
@@ -5,15 +5,15 @@ public class HaierServiceObject {
 
 	public static final String SERVICE_URL = "http://115.29.231.29/Haier/";
 	
-	public static final String PRODUCT_AVATOR_URL= "http://www.51cck.com/";
+	public static final String PRODUCT_AVATOR_URL= "http://115.29.231.29/proimg/";
 	/***
-	   * 返回商品预览图网址www.51cck.com/KY前9位数字/KY.jpg
+	   * 产品图片网址  http://115.29.231.29/proimg/507/5070A000A.jpg  说明5070A000A：为Key，507：key 为前三位
 	   * @return
 	   */
 	public static String getProdcutAvatorUrl(String ky) {
-		String ky9 = ky.substring(0,9);
+		String ky3 = ky.substring(0,3);
 		  StringBuilder sb = new StringBuilder(PRODUCT_AVATOR_URL);
-		  sb.append(ky9).append("/").append(ky).append(".jpg");
+		  sb.append(ky3).append("/").append(ky).append(".jpg");
 		  return sb.toString();
 	}
 }
