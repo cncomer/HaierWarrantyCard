4e91f09036f12bafc6c80c5dd40afde7d5518bc2
cncom <yeluosuifeng2005@gmail.com>
2014-05-15 02:44:42 +0800

    关闭DebugUtils的logD输出

--- src/com/bestjoy/app/haierwarrantycard/utils/DebugUtils.java
+++ src/com/bestjoy/app/haierwarrantycard/utils/DebugUtils.java
@@ -3,7 +3,7 @@ package com.bestjoy.app.haierwarrantycard.utils;
 import android.util.Log;
 
 public class DebugUtils {
-	public static final boolean DEBUG = true;
+	public static final boolean DEBUG = false;
 
 	public static void logD(String tag, String msg) {
 		if (DEBUG) Log.d(tag, "cncom " + msg);
